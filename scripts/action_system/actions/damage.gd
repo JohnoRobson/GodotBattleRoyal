1aa59613f3cc23b127cadd86a5fe1f7d8137dfaf
Johno Robson <johnathan.g.robson@gmail.com>
2023-10-15 20:07:52 -0700

    undid another change

--- scripts/action_system/actions/damage.gd
+++ scripts/action_system/actions/damage.gd
@@ -12,5 +12,5 @@ func perform(_delta: float, item_node: ActionStack.ItemNode) -> bool:
 	for target in targets:
 		if target is Actor:
 			var actor: Actor = target as Actor
-			actor.get_node("Rotator/Body/Hurtbox").take_damage(damage, item_node.data.get(self.Keys.POSITION), (actor.global_position - item_node.data.get(self.Keys.POSITION)).normalized())
+			actor.get_node("Hurtbox").take_damage(damage, item_node.data.get(self.Keys.POSITION), (actor.global_position - item_node.data.get(self.Keys.POSITION)).normalized())
 	return true

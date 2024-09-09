87e8f38542792df6c2b76ca5f6b120f04dc1a6f0
Morgan Wynne <morganwynne@outlook.com>
2024-09-08 20:52:10 -0700

    Removed unused is_health_nearby factor

--- scripts/ai/factor/factors.gd
+++ scripts/ai/factor/factors.gd
@@ -52,7 +52,7 @@ static func evaluate_loot_factor(factor_context: FactorContext) -> float:
 	var radius: float = 10.0
 	var world = factor_context.world
 	var items_in_area: Array = world.get_actors_and_gameitems_in_area(factor_context.target_position, radius).filter(func(a): return a is GameItem).filter(func(a): return a != null and !a.is_held and a.can_be_used)
-	
+
 	return clamp_result(items_in_area.size() * 0.2)
 
 # returns 0.0 to 1.0 based on how dangerous this actor's position is based off grenade proximity. 0.0 is safe, 1.0 is unsafe

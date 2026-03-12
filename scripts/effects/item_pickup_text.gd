a1331015a65d023ef855f6e1891f3bc64346dda4
Johno Robson <johnathan.g.robson@gmail.com>
2026-03-11 20:00:53 -0700

    Made the item pickup text correctly show if pressing F will pick up or swap an item. Cleaned up the inventory.gd script.

--- scripts/effects/item_pickup_text.gd
+++ scripts/effects/item_pickup_text.gd
@@ -2,19 +2,34 @@ class_name ItemPickupText
 extends Node3D
 
 var game_item: GameItem
+var text_state: InteractionText
 @onready var item_name_text: Label3D = get_node("ItemNameText")
-@onready var item_interact_text: Label3D = get_node("ItemInteractText")
+@onready var item_pickup_text: Label3D = get_node("ItemPickupText")
+@onready var item_swap_text: Label3D = get_node("ItemSwapText")
+
+enum InteractionText {
+	NONE, SWAP, PICK_UP
+}
 
 func _ready() -> void:
-	item_interact_text.visible = false
+	text_state = InteractionText.NONE
 
 func set_item(_game_item: GameItem) -> void:
 	game_item = _game_item
 	item_name_text.text = _game_item.item_name
 	global_position = _game_item.global_position + (Vector3.UP * 2)
 
-func set_show_interaction_text(_show: bool) -> void:
-	item_interact_text.visible = _show
+func _process(delta: float) -> void:
+	match text_state:
+		InteractionText.NONE:
+			item_pickup_text.hide()
+			item_swap_text.hide()
+		InteractionText.SWAP:
+			item_pickup_text.hide()
+			item_swap_text.show()
+		InteractionText.PICK_UP:
+			item_pickup_text.show()
+			item_swap_text.hide()
 
 func _physics_process(_delta) -> void:
 	if game_item != null && game_item.is_inside_tree():
